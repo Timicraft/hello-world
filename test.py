2fc74bb461809d324c30c56c54ac9451b09beba1
Timicraft <446230186@qq.com>
2018-09-13 21:24:41 +0800

    My First Python
    a hello world python

---
+++ test.py
@@ -0,0 +1,10 @@
+class test():
+    def say(self):
+        print("i say something")
+
+    def speak(self):
+        if not say():
+            print("no say")
+        else:
+            print("i have said something")
+
